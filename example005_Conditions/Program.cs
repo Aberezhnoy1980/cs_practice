7fc316ecf80460a8ef465512dea2c64034a80ccf
Aleksandr Berezhnoy <alex@MacBook-Pro-Aleksandr.local>
2023-07-03 07:15:34 +0300

    005 Condition

---
+++ example005_Conditions/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("type username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha") 
+{
+    Console.WriteLine("Hooray! This is Masha");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
