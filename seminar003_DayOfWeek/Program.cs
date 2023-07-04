34d626d8a20c85d15b5b36a35d3ba4cf93f167cf
Aleksandr Berezhnoy <alex@MacBook-Pro-Aleksandr.local>
2023-07-04 21:11:04 +0300

    seminar003

---
+++ seminar003_DayOfWeek/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+
+            Console.WriteLine("type day of the week number");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num > 0 && num <= 7)
+            {
+                Console.WriteLine(days[num - 1]);
+            }
+            else Console.WriteLine("input correct number from 1 to 7");
+        }
+    }
+}
