c976ec1405b49728c85e9549795a005245d52cf0
Aleksandr Berezhnoy <alex@MacBook-Pro-Aleksandr.local>
2023-07-04 22:25:55 +0300

    seminar004

---
+++ seminar004_task7/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input any positive integer");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num >= 0)
+            {
+                mirrorArrayCreate1(num);
+            }
+            else Console.WriteLine("incorrect number"); 
+        }
+
+        static void mirrorArrayCreate(int num)
+        {
+            int[] numbers = new int[num * 2 + 1];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i < numbers.Length / 2)
+                {
+                    numbers[i] = -(numbers.Length / 2 - i);
+                }
+                else numbers[i] = i - numbers.Length / 2;
+                Console.Write(numbers[i] + " ");
+            }
+            // Array.ForEach(numbers, Console.WriteLine);
+        }
+
+         static void mirrorArrayCreate1(int num)
+        {
+           num = Math.Abs(num);
+
+           int counter = -num;
+
+           while (counter <= num)
+           {
+            Console.Write(counter + " ");
+            counter++;
+           }
+        }
+    }
+}
