5f52b544af270c7c6a0daac54190359d6cda78ef
Aleksandr Berezhnoy <alex@MacBook-Pro-Aleksandr.local>
2023-07-07 13:12:34 +0300

    classwork 2 complete

---
+++ lesson002_ArrayLibrary/Program.cs
@@ -0,0 +1,64 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
+
+void GetSecondNumber()
+{
+    Console.WriteLine("input three-digit number");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number >= 100 && number < 1000)
+    {
+        int secondDigit = (number % 100 - number % 10) / 10;
+        Console.WriteLine("The second digit in " + number + " is " + secondDigit);
+    }
+    else Console.WriteLine("Your number isn't thhree-digit");
+}
+
+GetSecondNumber();
