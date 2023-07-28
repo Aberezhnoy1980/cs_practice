1eae211a8976e744eaf3dabee806fe0dbba51391
Aleksandr Berezhnoy <alex@MacBook-Pro-Aleksandr.local>
2023-07-28 10:38:25 +0300

    homework007 done

---
+++ homework007/MyLib.cs
@@ -0,0 +1,69 @@
+namespace MyLib;
+
+class MyLibClass
+{
+    public static int Promt(string message)
+    {
+        Console.WriteLine(message);
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    public static bool ValidateArray(int m, int n)
+    {
+        if (m < 0 || n < 0)
+            return false;
+        return true;
+    }
+
+    public static void FillArray(int[,] array, int minValue = -9, int maxValue = 10)
+    {
+        Random random = new Random();
+        for (int i = 0; i < array.GetLength(0); i++)
+            for (int j = 0; j < array.GetLength(1); j++)
+                array[i, j] = random.Next(minValue, maxValue);
+    }
+    public static void FillArray(double[,] array, int minValue = -9, int maxValue = 10)
+    {
+        Random random = new Random();
+        for (int i = 0; i < array.GetLength(0); i++)
+            for (int j = 0; j < array.GetLength(1); j++)
+                array[i, j] = Math.Round(random.NextDouble() * (maxValue - minValue) + minValue, 1);
+    }
+
+    public static void PrintArray(int[,] array)
+    {
+        Console.Write($"col index:   |\t");
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+            Console.Write($"{i} \t|\t");
+        }
+        Console.WriteLine();
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            Console.Write($"row index: {i} |\t");
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + "\t|\t");
+            }
+            Console.WriteLine();
+        }
+    }
+    public static void PrintArray(double[,] array)
+    {
+        Console.Write($"col index:   |\t");
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+            Console.Write($"{i} \t|\t");
+        }
+        Console.WriteLine();
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            Console.Write($"row index: {i} |\t");
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + "\t|\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ homework007/Program.cs
@@ -0,0 +1,119 @@
+﻿using MyLib;
+
+namespace homework007;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        void Task47()
+        {
+            int m = MyLibClass.Promt("Enter m size: ");
+            int n = MyLibClass.Promt("Enter n size: ");
+
+            double[,] doubles;
+            if (MyLibClass.ValidateArray(m, n))
+                doubles = new double[m, n];
+            else
+            {
+                Console.WriteLine("Incorrect sizes. Please try again");
+                return;
+            }
+
+            MyLibClass.FillArray(doubles);
+            MyLibClass.PrintArray(doubles);
+        }
+
+        // Task47();
+
+        void FindElementByIndex(int[,] array, int row, int col)
+        {
+            int value = 0;
+            if (row >= array.GetLength(0) || col >= array.GetLength(1) || row < 0 || col < 0)
+            {
+                Console.Write($"The element with indexes [{row}, {col}] is array out of bound");
+            }
+            else
+            {
+                Console.Write($"The element with indexes [{row}, {col}] is {array[row, col]}");
+            }
+        }
+
+        void Task50()
+        {
+            int m = MyLibClass.Promt("Enter m size: ");
+            int n = MyLibClass.Promt("Enter n size: ");
+
+            int[,] ints;
+            if (MyLibClass.ValidateArray(m, n))
+                ints = new int[m, n];
+            else
+            {
+                Console.WriteLine("Incorrect sizes. Please try again");
+                return;
+            }
+
+            MyLibClass.FillArray(ints);
+            MyLibClass.PrintArray(ints);
+
+            int row = MyLibClass.Promt("Enter row searching index: ");
+            int col = MyLibClass.Promt("Enter column searching index: ");
+
+            FindElementByIndex(ints, row, col);
+        }
+
+        // Task50();
+
+        void PrintAverages(double[] avgs)
+        {
+            Console.WriteLine("Average values for each column:");
+            Console.Write($"col index:   |\t");
+            for (int i = 0; i < avgs.Length; i++)
+            {
+                Console.Write($"{i} \t|\t");
+            }
+            Console.WriteLine();
+            Console.Write($"averages:    |\t");
+            for (int i = 0; i < avgs.Length; i++)
+                Console.Write(avgs[i] + "\t|\t");
+        }
+
+        double[] GetAverages(int[,] array)
+        {
+            double[] avgs = new double[array.GetLength(1)];
+            double sum = 0;
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    sum += array[i, j];
+                }
+                avgs[j] = Math.Abs(sum / array.GetLength(0));
+                sum = 0;
+            }
+            return avgs;
+        }
+
+        void Task52()
+        {
+            int m = MyLibClass.Promt("Enter m size: ");
+            int n = MyLibClass.Promt("Enter n size: ");
+
+            int[,] ints;
+            if (MyLibClass.ValidateArray(m, n))
+                ints = new int[m, n];
+            else
+            {
+                Console.WriteLine("Incorrect sizes. Please try again");
+                return;
+            }
+
+            MyLibClass.FillArray(ints);
+            MyLibClass.PrintArray(ints);
+
+            PrintAverages(GetAverages(ints));
+        }
+
+        Task52();
+    }
+}
